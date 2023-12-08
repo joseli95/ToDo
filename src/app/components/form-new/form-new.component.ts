7748c7708c92c2f5b7f7cc0d9f1c1167d6794a5e
joseli95 <josepepe.serrano3@gmail.com>
2023-12-08 15:30:21 -0600

    Validate new task form

--- src/app/components/form-new/form-new.component.ts
+++ src/app/components/form-new/form-new.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, Validators } from '@angular/forms'
 
 import '@ui5/webcomponents/dist/Button.js'
 import '@ui5/webcomponents/dist/Input.js'
+import '@ui5/webcomponents/dist/features/InputElementsFormSupport.js'
 import '@ui5/webcomponents/dist/Label.js'
 import '@ui5/webcomponents/dist/Card.js'
 import '@ui5/webcomponents/dist/CardHeader.js'
 import '@ui5/webcomponents/dist/DatePicker.js'
+import '@ui5/webcomponents/dist/MessageStrip.js'
 
 @Component({
   selector: 'app-form-new',
   templateUrl: './form-new.component.html',
   styleUrls: ['./form-new.component.css'],
 })
-export class FormNewComponent {}
+export class FormNewComponent {
+  private formBuilder = inject(FormBuilder)
+
+  showErrorMessage = false
+  errorMessage = ''
+
+  formNewTask = this.formBuilder.group({
+    task: ['', Validators.required],
+    date: ['', Validators.required],
+  })
+
+  saveTask() {
+    if (this.formNewTask.valid) {
+    } else {
+      this.showErrorMessage = true
+      this.errorMessage = 'Debes completar todos los campos'
+
+      setTimeout(() => {
+        this.showErrorMessage = false
+      }, 3500)
+    }
+  }
+}
--- src/app/pages/system/system.module.ts
+++ src/app/pages/system/system.module.ts
@@ -6,9 +6,16 @@ import { NewTaskComponent } from './new-task/new-task.component'
 import { FormNewComponent } from '../../components/form-new/form-new.component'
 import { TasksListComponent } from '../../components/tasks-list/tasks-list.component'
 import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx'
+import { ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
 
 @NgModule({
-  imports: [RouterModule.forChild(SystemRouting), Ui5WebcomponentsModule],
+  imports: [
+    RouterModule.forChild(SystemRouting),
+    Ui5WebcomponentsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   declarations: [NewTaskComponent, FormNewComponent, TasksListComponent],
 })
 export class SystemModule {}
